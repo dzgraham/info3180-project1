de540f599ae96011ea8f6a8ec8176405bd736343
Daniel Graham <dzgraham976@gmail.com>
2026-03-20 23:51:11 -0500

    Added routing for pages

--- app/views.py
+++ app/views.py
@@ -5,8 +5,13 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file contains the routes for your application.
 """
 
-from app import app
-from flask import render_template, request, redirect, url_for
+from app import app, db, login_manager
+from flask import render_template, flash, send_from_directory, request, redirect, url_for
+from flask_login import login_user, logout_user, current_user, login_required
+from app.forms import LoginForm, PropertyForm
+from app.models import UserProfile, Property
+from werkzeug.utils import secure_filename
+import os
 
 
 ###
@@ -22,8 +27,86 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Daniel Graham")
 
+
+@app.route('/properties/create', methods=['GET', 'POST'])
+@login_required
+def create_property():
+    form = PropertyForm()
+
+    if form.validate_on_submit():
+        photo = form.photo.data
+        filename = secure_filename(photo.filename)
+
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        photo.save(upload_path)
+
+        property = Property(
+            title=form.title.data,
+            description=form.description.data,
+            bedrooms=form.bedrooms.data,
+            bathrooms=form.bathrooms.data,
+            location=form.location.data,
+            price=form.price.data,
+            property_type=form.property_type.data,
+            photo_filename=filename,
+            user_id=current_user.id
+        )
+
+        db.session.add(property)
+        db.session.commit()
+
+        flash('Property successfully added!', 'success')
+        return redirect(url_for('properties'))
+
+    return render_template('create_property.html', form=form)
+
+@app.route('/properties')
+def properties():
+    """Display a list of all properties."""
+    properties_list = Property.query.all()
+    return render_template('properties.html', properties=properties_list)
+
+
+@app.route('/properties/<int:property_id>')
+def view_property(property_id):
+    """Display an individual property by ID."""
+    this_property = Property.query.get_or_404(property_id)
+    return render_template('view_property.html', property=this_property)
+
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    """Return an image from the uploads folder."""
+    upload_folder = os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER'])
+    return send_from_directory(upload_folder, filename)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('properties'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = UserProfile.query.filter_by(username=form.username.data).first()
+
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            flash('Successfully logged in!', 'success')
+            return redirect(url_for('properties'))
+        else:
+            flash('Invalid username or password.', 'danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out.', 'success')
+    return redirect(url_for('login'))
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -61,3 +144,5 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
